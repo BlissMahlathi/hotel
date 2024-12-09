51ccaa4345586a3c4861f287e5287daa544526be
BlissMahlathi <162612924+BlissMahlathi@users.noreply.github.com>
2024-12-09 21:57:09 +0000

    with logo

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/hotel/',
-})
+  plugins: [react()],
+  base: "/hotel/", // Use your repository name here
+});
